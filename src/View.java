0dc62068fe6fd0a271cb3aae3248f4351bbb097d
Basel <basil.taleb@gmail.com>
2018-05-19 15:22:11 +0200

    Update View.java
    Removed unused comment.

--- src/View.java
+++ src/View.java
@@ -10,75 +10,14 @@ public class View {
 	public String getLastInpu() {
 		return lastInput;
 	}
-	/*
-	public int showRideSelectionMenu(List<Attraction> rides) {
-		System.out.println("Please select a ride: ");
 
-		for(int i = 0; i < rides.size(); i++) {
-			Attraction ride = rides.get(i);
-			System.out.println(i + 1 + " " + ride.getName() 
-				+ " for €" + ride.getPrice() );
-		}
-		
-		String selection = scan.next();	
-		int selectionInt = -1;
-		InputEnum input = getInputType(selection);
-
-		switch (input) {
-			case Number:
-				selectionInt = Integer.parseInt(selection);
-				break;
-			case Quit:
-				Program.Quit();
-				break;
-			case Invalid:
-			default:
-				printInvalidSelectionMessage();
-				showRideSelectionMenu(rides);
-				break;
-		}
-		
-		
-		if(selectionInt > rides.size() || selectionInt < 0) {
-			printInvalidSelectionMessage();
-			showRideSelectionMenu(rides);
-		}
-		
-		selection = showRideActionMenu();
-		input = getInputType(selection);
-		
-		switch (input) {
-		case Number:
-			int i = Integer.parseInt(selection);
-			if(i == 2)
-				showRideSelectionMenu(rides);
-			else if(i == 1)
-				return selectionInt;
-			else
-				printInvalidSelectionMessage();
-			break;
-			
-		case Quit:
-			Program.Quit();
-			break;
-			
-		case Invalid:
-		default:
-			printInvalidSelectionMessage();
-			showRideSelectionMenu(rides);
-			break;
-	}
-		return -1;
-		
-	}
-	*/
 	public String showRideSelectionMenu(List<Attraction> rides) {
 		System.out.println("Please select a ride: ");
 
 		for(int i = 0; i < rides.size(); i++) {
 			Attraction ride = rides.get(i);
 			System.out.println(i + 1 + " " + ride.getName() 
-				+ " for €" + ride.getPrice() );
+				+ " for â‚¬" + ride.getPrice() );
 		}
 		lastInput = scan.next();
 		return lastInput;
